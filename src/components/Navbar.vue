f910f4713b54ec793580474a608302b0520d4462
Miguel Ramos <return.infox@gmail.com>
2021-06-18 11:48:35 -0400

    Proyecto Cli con Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
